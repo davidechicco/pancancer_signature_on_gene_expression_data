6216db5f10ba8eac7a633ab7bb6c13179f80d192
davidechicco <davide.chicco@gmail.com>
2021-05-03 10:55:41 -0400

    edited file of the signatures by including the articles references

---
+++ bin/cancer_prognosis_probesets_signatures.r
@@ -0,0 +1,32 @@
+    
+    
+#     “Hypoxia predicts poor prognosis in neuroblastoma patients and associates with biological mechanisms involved in telomerase
+#      activation and tumor microenvironment reprogramming” https://doi.org/10.3390/cancers12092343 
+    DCangelosi_neuroblastoma_103390cancers12092343_probesets_signature <- c("200738_s_at", "17356_s_at", "206686_at", "226452_at", "223172_s_at",  "223193_x_at",  "224314_s_at", "230630_at", "202022_at")
+
+#     Van Laar, R. An online gene expression assay for determining adjuvant therapy eligibility in patients with stage 2 or 3 colon cancer. 
+#     British Journal of Cancer 103, 1852–1857 (2010). https://doi.org/10.1038/sj.bjc.6605970
+#     Supplementary Table 1
+    VanLaar_colorectal_cancer_101038sjbjc6605970_probesets_signature <- c("1553954_at", "1554078_s_at", "1555832_s_at", "1555950_a_at", "1560089_at", "1560587_s_at", "1563796_s_at", "200006_at", "200632_s_at", "200665_s_at", "200827_at", "200838_at", "200839_s_at", "200931_s_at", "200983_x_at", "201012_at", "201141_at", "201170_s_at", "201185_at", "201261_x_at", "201289_at", "201323_at", "201422_at", "201426_s_at", "201578_at", "201590_x_at", "201666_at",
+    "201925_s_at", "201926_s_at", "201939_at", "201951_at", "202068_s_at", "202237_at", "202238_s_at", "202419_at", "202457_s_at", "202478_at", "202839_s_at", "202887_s_at", "202904_s_at", "202939_at", "202949_s_at", "203072_at", "203083_at", "203382_s_at", "203476_at", "203895_at", "204264_at", "204472_at", "204620_s_at", "204679_at", "205677_s_at", "205963_s_at", "207543_s_at", "207574_s_at", "208891_at", "208892_s_at",
+    "208893_s_at", "208918_s_at", "208961_s_at", "209043_at", "209101_at", "209184_s_at", "209185_s_at", "209193_at", "209345_s_at", "209386_at", "209387_s_at", "209457_at", "209545_s_at", "209624_s_at", "209711_at", "209875_s_at", "210095_s_at", "210275_s_at", "210427_x_at", "210495_x_at", "210512_s_at", "210517_s_at", "210592_s_at", "210652_s_at", "210845_s_at", "211074_at", "211719_x_at", "211924_s_at", "211928_at", "211988_at",
+    "212013_at", "212143_s_at", "212171_x_at", "212463_at", "212464_s_at", "212501_at", "212632_at", "212884_x_at", "213274_s_at", "213503_x_at", "213905_x_at", "214581_x_at", "214620_x_at", "214866_at", "215033_at", "215034_s_at", "215792_s_at", "216392_s_at", "216442_x_at", "217762_s_at", "217773_s_at", "217996_at", "218213_s_at", "218698_at", "218856_at", "218902_at", "219038_at", "219206_x_at", "219539_at", "221419_s_at",
+    "221479_s_at", "221563_at", "221648_s_at", "221656_s_at", "221730_at", "221731_x_at", "221745_at", "222421_at", "222994_at", "223003_at", "223122_s_at", "223163_s_at", "223312_at", "223454_at", "223455_at", "224602_at", "224606_at", "224657_at", "224777_s_at", "224806_at",
+    "224890_s_at", "224911_s_at", "225010_at", "225011_at", "225337_at", "225494_at", "225670_at", "225750_at", "226041_at", "226594_at", "226648_at", "226727_at", "226987_at", "227143_s_at", "227338_at", "227735_s_at", "227736_at", "227961_at", "229676_at", "231576_at", "234983_at", "241355_at", "242648_at", "35156_at", "36711_at", "58780_s_at")
+
+    #     Hallett, R., Dvorkin-Gheva, A., Bane, A. et al. A Gene Signature for Predicting Outcome in Patients with Basal-like Breast Cancer. 
+    #     Scientific Reports 2, 227 (2012). https://doi.org/10.1038/srep00227 
+    Hallett_breast_cancer_101038srep00227_probesets_signature  <- c("201022_s_at", "203072_at", "208089_s_at", "204338_s_at", "220719_at", "212039_x_at", "211073_x_at", "201217_x_at", "208538_at", "217434_at", "216143_at", "221306_at", "204544_at", "208885_at")
+    
+    #      Győrffy B, Surowiak P, Budczies J, Lánczky A (2013) Online Survival Analysis Software to Assess the Prognostic Value of Biomarkers
+    #      Using Transcriptomic Data in Non-Small-Cell Lung Cancer. PLoS ONE 8(12): e82241. https://doi.org/10.1371/journal.pone.0082241 
+    Gyorffy_lung_cancer_101371journalpone0082241_probesets_signature <- c("201243_s_at", "202490_at", "202707_at", "202814_s_at", "203001_s_at", "203147_s_at", "204179_at", "204584_at", "205386_s_at", "206426_at", "208399_s_at", "210016_at", "218881_s_at", "219171_s_at", "221591_s_at")
+
+#     Chen X, Xu S, McClelland M, Rahmatpanah F, Sawyers A, Jia Z, et al. (2012) An Accurate Prostate Cancer Prognosticator Using 
+#      a Seven-Gene Signature Plus Gleason Score and Taking Cell Type Heterogeneity into Account. PLoS ONE 7(9): e45178. 
+#     https://doi.org/10.1371/journal.pone.004517
+    Chen_prostate_cancer_101371journalpone004517_probesets_signature <- c("221523_s_at", "214527_s_at", "208490_x_at", "207016_s_at", "213293_s_at", "209487_at", "221667_s_at")
+
+# merging all signatures together
+all_signatures <- c(DCangelosi_neuroblastoma_103390cancers12092343_probesets_signature, VanLaar_colorectal_cancer_101038sjbjc6605970_probesets_signature, Hallett_breast_cancer_101038srep00227_probesets_signature, Gyorffy_lung_cancer_101371journalpone0082241_probesets_signature, Chen_prostate_cancer_101371journalpone004517_probesets_signature)
+
